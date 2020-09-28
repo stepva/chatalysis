02d076e737ce25bd302898cd77714acd6f6014ab
stepva <43886074+stepva@users.noreply.github.com>
2020-09-28 18:48:31 +0200

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,73 +1,193 @@
-import json
-from datetime import datetime
+import json, pprint, operator
+from datetime import datetime, date
 
+#add more for more chat.json files - and change their number (message_N) and (dataN)
+#REMEMBER TO DO THIS ON LINES 13, 121, 151, 163, 181
 with open("message_1.json") as chat:
     data = json.load(chat)
 
-ps = data["participants"]
-ms = data["messages"]
-total = len(ms)
-first = str(datetime.fromtimestamp(ms[-1]["timestamp_ms"]//1000))
-last = str(datetime.fromtimestamp(ms[0]["timestamp_ms"]//1000))
+"""with open("message_2.json") as chat:
+    data2 = json.load(chat)"""
+"""with open("message_3.json") as chat:
+    data3 = json.load(chat)
+with open("message_4.json") as chat:
+    data4 = json.load(chat)"""
 
-info = {
-    "from": first,
-    "to": last,
-    "total": total
-}
+#change "dataN" in fromDay to the highest data object
+fromDay = str(date.fromtimestamp(data["messages"][-1]["timestamp_ms"]//1000))
+toDay = str(date.fromtimestamp(data["messages"][0]["timestamp_ms"]//1000))
+title = data["title"]
 
 def getNames():
     ns = []
-    for i in range(len(ps)):
-        ns.append(ps[i]["name"])
+    for i in range(len(data["participants"])):
+        ns.append(data["participants"][i]["name"])
     return ns
-
-def msgCount(names):
-    for n in names:
-        info[n] = (sum(1 for i in range(total) if ms[i]["sender_name"]==n))
-    return info
-
-def imgCount(names):
-    sum_img = sum(1 for i in range(total) if "photos" in ms[i])
-    info["total_images"] = sum_img
-    if sum_img > 0:
-        for n in names:
-            info[n + " images"] = (sum(1 for i in range(total) if "photos" in ms [i] and ms[i]["sender_name"]==n))
-    return info
-
-def gifCount(names):
-    sum_gif = sum(1 for i in range(total) if "gifs" in ms[i])
-    info["total_gifs"] = sum_gif
-    if sum_gif > 0:
-        for n in names:
-            info[n + " gifs"] = (sum(1 for i in range(total) if "gifs" in ms [i] and ms[i]["sender_name"]==n))
-    return info
-
-def audioCount(names):
-    sum_aud = sum(1 for i in range(total) if "audio_files" in ms[i])
-    info["total_audio"] = sum_aud
-    if sum_aud > 0:
-        for n in names:
-            info[n + " audio"] = (sum(1 for i in range(total) if "audio_files" in ms [i] and ms[i]["sender_name"]==n))
-    return info
-
 names = getNames()
-msgCount(names)
-imgCount(names)
-gifCount(names)
-audioCount(names)
 
-print(json.dumps(info, indent=2))
-print("ahoj testuju github")
+def chatAlysis(dataObject, names):
+    ps = dataObject["participants"]
+    ms = dataObject["messages"]
+    total = len(ms)
 
+    info = {
+        "1) Total messages": total
+    }
 
-def imgs():
-    return sum(1 for i in range(total) if "photos" in ms[i])
-def gifs():
-    return sum(1 for i in range(total) if "gifs" in ms[i])
-def audios():
-    return sum(1 for i in range(total) if "audio_files" in ms[i])
-def contentCount():
-    return sum(1 for i in range(total) if "content" in ms[i])
-if audios()+gifs()+imgs()+contentCount()==total:
-    print("ano")
+    def msgCount(names):
+        for n in names:
+            num = sum(1 for i in range(total) if ms[i]["sender_name"]==n)
+            info[n] = num
+        return info
+
+    def imgCount(names):
+        sum_img = sum(1 for i in range(total) if "photos" in ms[i])
+        if sum_img > 0:
+            info["4) Total images"] = sum_img
+            for n in names:
+                info[n + " images"] = sum(1 for i in range(total) if "photos" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    def gifCount(names):
+        sum_gif = sum(1 for i in range(total) if "gifs" in ms[i])
+        if sum_gif > 0:
+            info["3) Total gifs"] = sum_gif
+            for n in names:
+                info[n + " gifs"] = sum(1 for i in range(total) if "gifs" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    def vidCount(names):
+        sum_vid = sum(1 for i in range(total) if "videos" in ms[i])
+        if sum_vid > 0:
+            info["6) Total videos"] = sum_vid
+            for n in names:
+                info[n + " videos"] = sum(1 for i in range(total) if "videos" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    def stiCount(names):
+        sum_sti = sum(1 for i in range(total) if "sticker" in ms[i])
+        if sum_sti > 0:
+            info["5) Total stickers"] = sum_sti
+            for n in names:
+                info[n + " stickers"] = sum(1 for i in range(total) if "sticker" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    def audioCount(names):
+        sum_aud = sum(1 for i in range(total) if "audio_files" in ms[i])
+        if sum_aud > 0:
+            info["7) Total audios"] = sum_aud
+            for n in names:
+                info[n + " audio"] = sum(1 for i in range(total) if "audio_files" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    def fileCount(names):
+        sum_fil = sum(1 for i in range(total) if "files" in ms[i])
+        if sum_fil > 0:
+            info["2) Total files"] = sum_fil
+            for n in names:
+                info[n + " files"] = sum(1 for i in range(total) if "files" in ms [i] and ms[i]["sender_name"]==n)
+        return info
+
+    msgCount(names)
+    imgCount(names)
+    gifCount(names)
+    vidCount(names)
+    stiCount(names)
+    audioCount(names)
+    fileCount(names)
+
+    #pprint.pprint(final, indent=2, sort_dicts=False)
+
+    def vibeCheck():
+        sum_con = sum(1 for i in range(total) if "content" in ms[i])
+        sum_img = sum(1 for i in range(total) if "photos" in ms[i])
+        sum_gif = sum(1 for i in range(total) if "gifs" in ms[i])
+        sum_sti = sum(1 for i in range(total) if "sticker" in ms[i])
+        sum_vid = sum(1 for i in range(total) if "videos" in ms[i])
+        sum_aud = sum(1 for i in range(total) if "audio_files" in ms[i])
+        sum_fil = sum(1 for i in range(total) if "files" in ms[i])
+        sumsum = sum_con + sum_img + sum_gif + sum_vid + sum_aud + sum_fil + sum_sti
+        if sumsum == total:
+            return "ok"
+        else:
+            print("something’s wrong: " + str(sumsum))
+    vibeCheck()
+
+    return info
+
+def deCode(dictx):
+    final = {}
+    for k in dictx:
+        final[k.encode('iso-8859-1').decode('utf-8')] = dictx[k]
+    return final
+
+#add more for more data objects and change their "data_" number
+chats = [chatAlysis(data, names)]
+
+def getResult(chatRess):
+    return {k: sum(t.get(k, 0) for t in chatRess) for k in set.union(*[set(t) for t in chatRess])}
+result = getResult(chats)
+
+result["0) Chat: " + title] = fromDay + " to " + toDay
+for n in names:
+    result["{0} %".format(n)] = str(round(result[n]/result["1) Total messages"]*100, 2)) + " %"
+
+finale = deCode(result)
+pprint.pprint(finale, indent=2, sort_dicts=True)
+
+def topDay(dataO):
+    topD = 0
+    countD = 0
+    dayX = date.fromtimestamp(dataO["messages"][0]["timestamp_ms"]//1000)
+    countX = 0
+    for i in range(len(dataO["messages"])):
+        dayY = date.fromtimestamp(dataO["messages"][i]["timestamp_ms"]//1000)
+        if dayY == dayX:
+            countX += 1
+            if countX > countD:
+                countD = countX
+                topD = dayX
+        else:
+            countX = 1
+            dayX = dayY
+    return [topD, countD]
+#add more for more data objects and change their number
+topDays = [topDay(data)]
+theTopDay = max(topDays, key=lambda x: x[1])
+print("The top day was " + str(theTopDay[0]) + " with " + str(theTopDay[1]) + " messages.")
+
+def dayStats(dataO):
+    days = {}
+    for d in range(1,8):
+        msgD = sum(1 for i in range(len(dataO["messages"])) if date.fromtimestamp(dataO["messages"][i]["timestamp_ms"]//1000).isoweekday()==d)
+        if msgD > 0:
+            days["{0}".format(d)] = msgD
+    return days
+#add more for more data objects and change their number
+daySts = [dayStats(data)]
+dayStsSum = getResult(daySts)
+def topDoW(dayss):
+    dNames = {"1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday", "7": "Sunday"}
+    dayDay = max(dayss.items(), key=operator.itemgetter(1))[0]
+    dayDayN = dNames[dayDay]
+    dayDayC = dayss[dayDay]
+    print("Most messages were sent on " + dayDayN + "s - " + str(dayDayC) + ".")
+topDoW(dayStsSum)
+
+def monthStats(dataO):
+    months = {}
+    for m in range(1,12):
+        msgM = sum(1 for i in range(len(dataO["messages"])) if date.fromtimestamp(dataO["messages"][i]["timestamp_ms"]//1000).month==m)
+        if msgM > 0:
+            months["{0}".format(m)] = msgM
+    return months
+#add more for more data objects and change their number
+monSts = [monthStats(data)]
+monStsSum = getResult(monSts)
+def topMoY(monthss):
+    dNames = {"1": "Januray", "2": "February", "3": "March", "4": "April", "5": "May", "6": "June", "7": "July", "8": "August", "9": "September", "10": "October", "11": "November", "12": "December"}
+    monthMonth = max(monthss.items(), key=operator.itemgetter(1))[0]
+    monthMonthN = dNames[monthMonth]
+    monthMonthC = monthss[monthMonth]
+    print("Most messages were sent in " + monthMonthN + " - " + str(monthMonthC) + ".")
+topMoY(monStsSum)
