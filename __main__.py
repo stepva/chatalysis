a020b416f7b0f861272883c22cdad8a5a9170b30
Štěpán Vácha <step.vacha@gmail.com>
2020-10-24 13:41:54 +0100

    🔥  Made lit changes to structure

---
+++ __main__.py
@@ -0,0 +1,66 @@
+#standard imports:
+import json
+import argparse
+import os
+from datetime import datetime, date
+from pprint import pprint
+#pakcage imports:
+import emoji
+import regex
+import matplotlib.pyplot as plt
+#local imports:
+from analysis import *
+
+version = "0.69"
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-V', '-version', '--version',
+                        help='Version', action='version', version=version)
+    parser.add_argument("chat", help="Chat Name")
+    args = parser.parse_args(argv)
+
+    home = os.getcwd()
+    chats = []
+    for d in os.listdir(home):
+        if d.startswith("messages") and os.path.isdir(f"{home}/{d}"):
+            for f in os.listdir(d + "/inbox"):
+                if f.startswith(args.chat + "_"):
+                    chats.append(f"{d}/inbox/{f}")
+
+    if not chats:
+        raise Exception("NO CHATS NAMED " + args.chat)
+
+    jsons = []
+    for ch in chats:
+        for file in os.listdir(ch):
+            if file.startswith("message") and file.endswith(".json"):
+                jsons.append(f"{ch}/{file}")
+
+    if not jsons:
+        raise Exception("NO JSON FILES IN THIS CHAT")
+
+    with open(jsons[0]) as chat:
+        data = json.load(chat)
+        title = decode(data["title"])
+        names = getNames(data)
+
+    messages = []
+    for j in jsons:
+        with open(j) as data:
+            data = json.load(data)
+            messages.extend(data["messages"])
+    messages = sorted(messages, key=lambda k: k["timestamp_ms"])
+    decodeMsgs(messages)
+
+    basicStats, reactions, emojis, times, people = raw(messages, names)
+
+    header(title, messages)
+    pprint(chatStats(basicStats, names), indent=2, sort_dicts=True)
+    pprint(reactionStats(reactions, names, people), indent=2, sort_dicts=False)
+    pprint(emojiStats(emojis, names, people), indent=2, sort_dicts=False)
+    pprint(timeStats(times), indent=2, sort_dicts=False)
+    pprint(firstMsg(messages), indent=2, sort_dicts=False)
+
+if __name__ == "__main__":
+    main()
--- analysis.py
+++ analysis.py
@@ -1,64 +1,11 @@
-#TODO: argparse, WORK ON GRAPHS, 
+#TODO: WORK ON GRAPHS, infographic, INTERFACE
 #ideas: allchatalysis
 
-import json, argparse, sys, os, emoji, regex, matplotlib.pyplot as plt
+import emoji
+import regex
+import matplotlib.pyplot as plt
 from datetime import datetime, date
-from pprint import pprint
 
-def main(argv=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-V', '-version', '--version', help='Version', action='version', version='v0.69')
-    parser.add_argument("chat", help="Chat Name")
-    args = parser.parse_args(argv)
-
-    home = os.getcwd()
-    chats = []
-    for d in os.listdir(home):
-        if d.startswith("messages") and os.path.isdir(f"{home}/{d}"):
-            for f in os.listdir(d + "/inbox"):
-                if f.startswith(args.chat + "_"):
-                    chats.append(f"{d}/inbox/{f}")
-
-    if not chats:
-        raise Exception("NO CHATS NAMED " + args.chat)
-
-    jsons = []
-    for ch in chats:
-        for file in os.listdir(ch):
-            if file.startswith("message") and file.endswith(".json"):
-                jsons.append(f"{ch}/{file}")
-
-    if not jsons:
-        raise Exception("NO JSON FILES IN THIS CHAT")
-
-    with open(jsons[0]) as chat:
-        data = json.load(chat)
-        title = decode(data["title"])
-        names = getNames(data)
-    
-    messages = []
-    for j in jsons:
-        with open(j) as data:
-            data = json.load(data)
-            messages.extend(data["messages"])
-    messages = sorted(messages, key=lambda k: k["timestamp_ms"])     
-    decodeMsgs(messages)
-        
-    basicStats, reactions, emojis, times, people = raw(messages, names)
-
-    header(title, messages)
-    pprint(chatStats(basicStats, names), indent=2, sort_dicts=True)
-    pprint(reactionStats(reactions, names, people), indent=2, sort_dicts=False)
-    pprint(emojiStats(emojis, names, people), indent=2, sort_dicts=False)
-    pprint(timeStats(times), indent=2, sort_dicts=False)
-    pprint(firstMsg(messages), indent=2, sort_dicts=False)   
-    
-
-def isDirPath(path):
-  if os.path.isdir(path):
-    return path
-  else:
-    raise argparse.ArgumentTypeError(f"invalid dir path: {path}")
 
 def raw(messages, names):
     people = {"total": 0}
@@ -312,6 +259,3 @@ def graphBarhReacts(stats):
     plt.barh(types, values)
     #plt.yticks(types)
     plt.show()
-
-if __name__ == "__main__":
-    main()
