8af3e432d28036f5e3e04502a6dbfc927e0fcb42
Štěpán Vácha <step.vacha@gmail.com>
2021-03-15 23:41:09 +0100

    🐛  Version fix

--- chatalysis/__init__.py
+++ chatalysis/__init__.py
@@ -6,3 +6,4 @@ Version logs and changes:
 - *v1.0.2*: fixed and updated for a new version of the emoji library
 - *v1.0.1*: added error message for no messages folder and improved HTML visualization when there are no emojis/reactions
 - *v1*: published!
+"""
