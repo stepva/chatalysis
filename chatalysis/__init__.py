9fa6e336a2efdf5a609902609ea7aab3f6d87a1f
Štěpán Vácha <43886074+stepva@users.noreply.github.com>
2022-07-24 17:46:14 +0200

    Remove version changes from __init__.py

--- chatalysis/__init__.py
+++ chatalysis/__init__.py
@@ -1,10 +1,2 @@
 # Version number
 __version__ = "v1.0.3"
-
-"""
-Version logs and changes:
-- *v1.0.3*: added a simple GUI
-- *v1.0.2*: fixed and updated for a new version of the emoji library
-- *v1.0.1*: added error message for no messages folder and improved HTML visualization when there are no emojis/reactions
-- *v1*: published!
-"""
