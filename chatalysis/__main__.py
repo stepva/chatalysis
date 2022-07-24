56242263cb28ad4d0095266ffcbab4b3d4ae8099
Štěpán Vácha <step.vacha@gmail.com>
2022-07-24 14:15:05 +0200

    ✨  keep CLI under optional argparse

--- chatalysis/__main__.py
+++ chatalysis/__main__.py
@@ -1,9 +1,32 @@
+# Standard library imports
+import argparse
+import sys
+
+# Application imports
 from program import Program
+from cli import cli
+from __init__ import version
 
-def main():
-    p = Program()
-    p.run()
 
-    
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-V", "-version", help="Version info", action="version", version=version
+    )
+
+    subparses = parser.add_subparsers()
+    parser_cli = subparses.add_parser("cli", help="Use CLI instead of GUI")
+    parser_cli.set_defaults(func=cli)
+
+    args = parser.parse_args(argv)
+
+    if "cli" in sys.argv:
+        args.func()
+
+    else:
+        p = Program()
+        p.run()
+
+
 if __name__ == "__main__":
     main()
---
+++ chatalysis/cli.py
@@ -0,0 +1,56 @@
+# Standard library imports
+import os
+
+# Application imports
+from pprint import pprint
+from analysis import topTen
+from utility import checkMedia, getMessageFolders, identifyChats
+from __init__ import version
+from chatalysis import htmllyse
+
+
+def cli():
+    path = os.getcwd()
+    folders = getMessageFolders(path)
+    chats = identifyChats(folders)
+    if not folders:
+        print(
+            'Looks like there is no messages folder here. Make sure to add the "messages" folder downloaded from Facebook to the chatalysis parent folder. More info in the README :)'
+        )
+        exit()
+
+    tops = None
+    i = ""
+
+    print("************************************")
+    print(f"Welcome to Chatalysis {version}!\n")
+    checkMedia(folders)
+    print(
+        """To see your Top 10 chats, just type \"top\"
+To chatalyse a specific conversation, just say which one - \"namesurname\"
+If you need help, read the README
+To exit, just type \"exit\":
+        """
+    )
+
+    while i != "exit":
+        print("What do you want to do?")
+        i = input()
+        chat = chats.get(i)
+
+        if i == "top":
+            print("\nLoading...\n")
+            if not tops:
+                tops = topTen(path)
+
+            pprint(tops, indent=2, sort_dicts=False)
+
+        elif chat is not None:
+            print("\nLoading...\n")
+            htmllyse(chat, folders)
+            print("Done. You can find it in the output folder!\n")
+
+        elif i != "exit":
+            print(f"No chats named {i}. Try again.")
+
+    return
