69731440289b5846e06525cdd82d9cda7db68d15
Štěpán Vácha <step.vacha@gmail.com>
2022-09-11 20:25:40 +0200

    💄 CLI refactor

--- chatalysis/__main__.py
+++ chatalysis/__main__.py
@@ -4,24 +4,23 @@ import sys
 
 # Application imports
 from program import Program
-from cli import cli
 from __init__ import __version__
 
 
-def main(argv=None):
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-V", "-version", help="Version info", action="version", version=__version__)
+    parser.add_argument(
+        "-V", "-version", help="Version info", action="version", version=__version__
+    )
 
     subparses = parser.add_subparsers()
-    parser_cli = subparses.add_parser("cli", help="Use CLI instead of GUI")
-    parser_cli.set_defaults(func=cli)
+    subparses.add_parser("cli", help="Use CLI instead of GUI")
 
-    args = parser.parse_args(argv)
+    p = Program()
 
     if "cli" in sys.argv:
-        args.func()
+        p.run(cli=True)
     else:
-        p = Program()
         p.run()
 
 
--- chatalysis/program.py
+++ chatalysis/program.py
@@ -1,8 +1,11 @@
 import io
+import os
 from pprint import pprint
 from gui import MainGUI
 from analyzer import Analyzer
 from utility import open_html, get_file_path, check_if_create_new
+from messenger import FacebookMessenger
+from __init__ import __version__
 
 
 class Program:
@@ -12,10 +15,14 @@ class Program:
         self.top_five_groups = None
         self.data_dir_path = ""
         self.valid_dir = False
-        self.gui = MainGUI(self)
+        self.gui = None
 
-    def run(self):
-        self.gui.mainloop()
+    def run(self, cli: bool = False):
+        if cli:
+            self.cli()
+        else:
+            self.gui = MainGUI(self)
+            self.gui.mainloop()
 
     def to_html(self, chat_name: str):
         """Analyzes the chat and creates an HTML output file"""
@@ -40,8 +47,70 @@ class Program:
         analyzer = Analyzer(chat)
 
         print(f"Chat: {chat.title}, from {chat.from_day} to {chat.to_day}")
-        pprint(analyzer.chat_stats(chat.basic_stats, chat.names), indent=2, sort_dicts=True)
-        pprint(analyzer.reaction_stats(chat.reactions, chat.names, chat.people), indent=2, sort_dicts=False)
-        pprint(analyzer.emoji_stats(chat.emojis, chat.names, chat.people), indent=2, sort_dicts=False)
+        pprint(
+            analyzer.chat_stats(chat.basic_stats, chat.names), indent=2, sort_dicts=True
+        )
+        pprint(
+            analyzer.reaction_stats(chat.reactions, chat.names, chat.people),
+            indent=2,
+            sort_dicts=False,
+        )
+        pprint(
+            analyzer.emoji_stats(chat.emojis, chat.names, chat.people),
+            indent=2,
+            sort_dicts=False,
+        )
         pprint(analyzer.time_stats(chat.times), indent=2, sort_dicts=False)
-        pprint(self.source.first_message(chat.messages), indent=2, sort_dicts=False)
+        pprint(chat.first_message(), indent=2, sort_dicts=False)
+
+    def cli(self):
+        """CLI loop used mainly for testing purposes"""
+        self.data_dir_path = os.getcwd()
+        try:
+            self.source = FacebookMessenger(self.data_dir_path)
+        except Exception as e:
+            print(f"Not a valid source - {e}")
+            return
+        folders = self.source.folders
+        chats = self.source.chat_ids
+        if not folders:
+            print(
+                'Looks like there is no messages folder here. Make sure to add the "messages" folder downloaded from Facebook to the chatalysis parent folder. More info in the README :)'
+            )
+            exit()
+
+        tops = None
+        i = ""
+
+        print("************************************")
+        print(f"Welcome to Chatalysis {__version__}!\n")
+        print(
+            """To see your Top 10 chats, just type \"top\"
+    To chatalyse a specific conversation, just say which one - \"namesurname\"
+    If you need help, read the README
+    To exit, just type \"exit\":
+            """
+        )
+
+        while i != "exit":
+            print("What do you want to do?")
+            i = input()
+            chat = chats.get(i)
+
+            if i == "top":
+                print("\nLoading...\n")
+                if not tops:
+                    tops = self.source.top_ten()
+
+                pprint(tops, indent=2, sort_dicts=False)
+
+            elif chat is not None:
+                print("\nLoading...\n")
+                self.to_html(i)
+                self.to_cli(i)
+                print("Done. You can find it in the output folder!\n")
+
+            elif i != "exit":
+                print(f"No chats named {i}. Try again.")
+
+        return
