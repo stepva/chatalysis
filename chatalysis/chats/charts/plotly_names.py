87ae5b06305b92e687b6c9166a2c3e2aa78e65f2
Štěpán Vácha <step.vacha@gmail.com>
2022-11-20 16:39:32 +0100

    🚧 piplackaaaa

--- chatalysis/chats/charts/plotly_names.py
+++ chatalysis/chats/charts/plotly_names.py
@@ -15,7 +15,8 @@ def groupchat_names_plot(group_names: list[dict[str, Any]], from_day: date, to_d
     df["from"] = df["timestamp"].apply(lambda x: datetime.fromtimestamp(x // 1000))
     df["to"] = df["timestamp"].apply(lambda x: datetime.fromtimestamp(x // 1000)).shift(-1)
     df["to"] = df["to"].fillna(pd.to_datetime(to_day))
-    df["to_str"] = df["to"].dt.strftime("%-m/%-d/%Y")  # for some reason the d3 formatting did not work for this column
+    df["from_str"] = df["from"].dt.strftime("%Y-%m-%d")
+    df["to_str"] = df["to"].dt.strftime("%Y-%m-%d")  # for some reason the d3 formatting did not work for this column
     df["type"] = "Chat name"
 
     fig1 = px.timeline(
@@ -35,7 +36,7 @@ def groupchat_names_plot(group_names: list[dict[str, Any]], from_day: date, to_d
             "hsla(45, 98%, 67%, 0.8)",
         ],
         text="group_name",
-        custom_data=["from", "to_str", "group_name", "changed_by"],
+        custom_data=["from_str", "to_str", "group_name", "changed_by"],
         height=100,
         range_x=[from_day, to_day],
     )
@@ -48,7 +49,7 @@ def groupchat_names_plot(group_names: list[dict[str, Any]], from_day: date, to_d
     fig1.update_traces(
         textposition="inside",
         insidetextanchor="start",
-        hovertemplate="<b>%{customdata[2]}</b><br>%{customdata[0]|%x} - %{customdata[1]}<br>Changed by %{customdata[3]}<extra></extra>",
+        hovertemplate="<b>%{customdata[2]}</b><br>%{customdata[0]} - %{customdata[1]}<br>Changed by %{customdata[3]}<extra></extra>",
     )
     fig1.update_yaxes(visible=False)
     fig1.update_yaxes(fixedrange=True)
@@ -58,7 +59,7 @@ def groupchat_names_plot(group_names: list[dict[str, Any]], from_day: date, to_d
     fig.update_layout(
         title={
             "text": "Chat names over time",
-            "y": 0.95,
+            "y": 1,
             "x": 0.5,
             "xanchor": "center",
             "yanchor": "top",
@@ -80,7 +81,8 @@ def nicknames_plot(nicknames: list[dict[str, Any]], from_day: date, to_day: date
     df["to"] = df.groupby("target")["timestamp"].shift(-1)
     df.loc[~df["to"].isna(), "to"] = df.loc[~df["to"].isna(), "to"].apply(lambda x: datetime.fromtimestamp(x // 1000))
     df["to"] = df["to"].fillna(pd.to_datetime(to_day))
-    df["to_str"] = df["to"].dt.strftime("%-m/%-d/%Y")  # for some reason the d3 formatting did not work for this column
+    df["from_str"] = df["from"].dt.strftime("%Y-%m-%d")
+    df["to_str"] = df["to"].dt.strftime("%Y-%m-%d")  # for some reason the d3 formatting did not work for this column
     df["type"] = "nickname"
     n_targets = len(df["target"].unique())
 
@@ -101,11 +103,10 @@ def nicknames_plot(nicknames: list[dict[str, Any]], from_day: date, to_day: date
             "hsla(45, 98%, 67%, 0.8)",
         ],
         text="nickname",
-        custom_data=["from", "to_str", "nickname", "changed_by"],
+        custom_data=["from_str", "to_str", "nickname", "changed_by"],
         range_x=[max([from_day, datetime(2015, 12, 18).date()]), to_day],
-        height=min([75 * n_targets, 250]),
+        height=max(75, min([50 * n_targets, 250])) if n_targets < 10 else n_targets * 25,
     )
-
     fig1.update_layout(
         showlegend=False,
         plot_bgcolor="rgba(0, 0, 0, 0)",
@@ -115,7 +116,7 @@ def nicknames_plot(nicknames: list[dict[str, Any]], from_day: date, to_day: date
     fig1.update_traces(
         textposition="inside",
         insidetextanchor="start",
-        hovertemplate="<b>%{customdata[2]}</b><br>%{customdata[0]|%x} - %{customdata[1]}<br>Changed by %{customdata[3]}<extra></extra>",
+        hovertemplate="<b>%{customdata[2]}</b><br>%{customdata[0]} - %{customdata[1]}<br>Changed by %{customdata[3]}<extra></extra>",
     )
     y_fix_range = True if n_targets <= 5 else False
     fig1.update_yaxes(fixedrange=y_fix_range)
@@ -125,7 +126,7 @@ def nicknames_plot(nicknames: list[dict[str, Any]], from_day: date, to_day: date
     fig.update_layout(
         title={
             "text": "Nicknames over time",
-            "y": 0.95,
+            "y": 1,
             "x": 0.5,
             "xanchor": "center",
             "yanchor": "top",
