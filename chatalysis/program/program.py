9e405889bde81e32b9dc728d82afcde6a0771239
Filip Miskarik <miskfilip@gmail.com>
2022-12-17 21:30:15 +0100

    Removed CLI

--- chatalysis/program/program.py
+++ chatalysis/program/program.py
@@ -1,13 +1,9 @@
 import io
-import os
-from pprint import pprint
 from typing import Any
 
-from __init__ import __version__
 from chats.analyzer import Analyzer
 from chats.stats import Stats
 from gui.main_gui import MainGUI
-from sources.messenger import Messenger
 from utils.utility import get_file_path, open_html
 from utils.config import Config
 
@@ -23,12 +19,9 @@ class Program:
         self.data_dir_path = ""
         self.valid_dir = False
 
-    def run(self, cli: bool = False) -> None:
-        if cli:
-            self.cli()
-        else:
-            self.gui = MainGUI(self)
-            self.gui.mainloop()
+    def run(self) -> None:
+        self.gui = MainGUI(self)
+        self.gui.mainloop()
 
     def chat_to_html(self, name: str) -> Any:
         chat = self.source.get_chat(name)
@@ -48,67 +41,3 @@ class Program:
             data.write(source)
 
         open_html(file_path)
-
-    def to_cli(self, chat_name: str) -> None:
-        """Analyzes the chat and prints the stats to terminal"""
-        chat = self.source.get_chat(chat_name)
-        analyzer = Analyzer(chat)
-
-        print(f"Chat: {chat.title}, from {chat.from_day} to {chat.to_day}")
-        pprint(analyzer.chat_stats(chat.participants), indent=2, sort_dicts=True)
-        pprint(analyzer.reaction_stats(chat.reactions, chat.names, chat.people), indent=2, sort_dicts=False)
-        pprint(analyzer.emoji_stats(chat.emojis, chat.names, chat.people), indent=2, sort_dicts=False)
-        pprint(analyzer.time_stats(chat.times), indent=2, sort_dicts=False)
-        pprint(chat.first_message(), indent=2, sort_dicts=False)
-
-    def cli(self) -> None:
-        """CLI loop used mainly for testing purposes"""
-        self.data_dir_path = os.getcwd()
-        try:
-            self.source = Messenger(self.data_dir_path)
-        except Exception as e:
-            print(f"Not a valid source - {e}")
-            return
-        folders = self.source.folders
-        chats = self.source.chat_ids
-        if not folders:
-            print(
-                'Looks like there is no messages folder here. Make sure to add the "messages" folder downloaded from Facebook to the chatalysis parent folder. More info in the README :)'
-            )
-            exit()
-
-        tops = None
-        i = ""
-
-        print("************************************")
-        print(f"Welcome to Chatalysis {__version__}!\n")
-        print(
-            """To see your Top 10 chats, just type \"top\"
-    To chatalyse a specific conversation, just say which one - \"namesurname\"
-    If you need help, read the README
-    To exit, just type \"exit\":
-            """
-        )
-
-        while i != "exit":
-            print("What do you want to do?")
-            i = input()
-            chat = chats.get(i)
-
-            if i == "top":
-                print("\nLoading...\n")
-                if not tops:
-                    tops = self.source.top_ten()
-
-                pprint(tops, indent=2, sort_dicts=False)
-
-            elif chat is not None:
-                print("\nLoading...\n")
-                self.to_html(i)
-                self.to_cli(i)
-                print("Done. You can find it in the output folder!\n")
-
-            elif i != "exit":
-                print(f"No chats named {i}. Try again.")
-
-        return
