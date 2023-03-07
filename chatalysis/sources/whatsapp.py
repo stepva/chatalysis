1237809b5b5a825382d87fda5fb0bb3ca02295dc
Filip Miskarik <miskfilip@gmail.com>
2023-03-07 16:51:59 +0100

    Added WhatsApp emoji processing

--- chatalysis/sources/whatsapp.py
+++ chatalysis/sources/whatsapp.py
@@ -1,10 +1,12 @@
 import datetime
 import regex
+import emoji
 from collections import defaultdict
 
 from chats.stats import Times, SourceType, StatsType
 from sources.message_source import MessageSource
 from chats.stats import Stats
+from utils.const import EMOJIS_REGEX, EMOJIS_DICT, TRANSLATE_REMOVE_LETTERS
 
 
 class WhatsApp(MessageSource):
@@ -13,6 +15,8 @@ class WhatsApp(MessageSource):
     def __init__(self, path: str):
         MessageSource.__init__(self, path)
         self._regex_whatsapp = regex.compile(r"([0-9]*)/([0-9]*)/([0-9]*), ([0-9]*):[0-9]* - ([^:]*):(.*)")
+        self._regex_emoji = regex.compile(EMOJIS_REGEX)
+
         self._messages = self._process_messages()
 
     # region Public API
@@ -39,8 +43,10 @@ class WhatsApp(MessageSource):
         with open(self._data_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
-        people: dict[str, int] = defaultdict(int)
+        name = ""
         participants = set()
+        people: dict[str, int] = defaultdict(int)
+        emojis: dict[str, dict[str, int]] = defaultdict(lambda: defaultdict(int))
 
         days: dict[str, int] = defaultdict(int)
         months: dict[str, int] = defaultdict(int)
@@ -67,6 +73,25 @@ class WhatsApp(MessageSource):
                 people[name] += 1
                 participants.add(name)
 
+            data = line.translate(TRANSLATE_REMOVE_LETTERS)
+
+            text_emoji = self._regex_emoji.search(data)
+            if text_emoji:
+                data += "".join([EMOJIS_DICT[e] for e in text_emoji.groups()])
+
+            # FIXME the emojis dict has a different structure than the Messenger one.
+            #   The Analyzer class needs to be modified to support this dict structure.
+
+            # Remaining lines of multiline messages are not matched by the regex (only the first line is)
+            # but their content is still analyzed for emoji and the emoji are attributed to the last name
+            # matched by the regex. There might be some edge case that will break this, but it works for now.
+            for c in data:
+                if c in emoji.EMOJI_DATA:
+                    emojis["total"]["total"] += 1
+                    emojis["total"][c] += 1
+                    emojis[name]["total"] += 1
+                    emojis[name][c] += 1
+
         times = Times(hours, days, weekdays, months, years)
         stats_type = StatsType.PERSONAL if len(participants) == 2 else StatsType.GROUP
 
@@ -79,7 +104,7 @@ class WhatsApp(MessageSource):
             audios=None,
             files=None,
             reactions=None,
-            emojis=None,
+            emojis=emojis,
             times=times,
             from_day=dates[0],
             to_day=dates[-1],
